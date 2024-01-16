59c5a636065687fd612a4ccc04c74bb2b5770941
ia.strigin <i-strigin1@mail.ru>
2024-01-16 17:54:06 +0300

    shepa-edit

--- Archives/schepa-ph/src/Componenets/Card.tsx
+++ Archives/schepa-ph/src/Componenets/Card.tsx
@@ -6,6 +6,10 @@ const Card = ({card}: {card: ICard}): ReactElement => {
     return (
         <div className={Styles.Container}>
             <img className={Styles.Img} src={card.img} />
+            <div className={Styles.TextContent}>
+                <span>Фракция: {card.fraction}</span>
+                <span>Фасовка: мешок {card.displacement}</span>
+            </div>
         </div>
     );
 };
